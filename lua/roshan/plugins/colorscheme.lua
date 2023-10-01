e9b440c269a79bf2da3751c8c56cafa3293562f3
Rossub <rosh2060an@gmail.com>
2023-10-01 13:49:30 -0400

    Added some themes

--- lua/roshan/plugins/colorscheme.lua
+++ lua/roshan/plugins/colorscheme.lua
@@ -19,7 +19,7 @@ return {
 			priority = 1000,
 			config = function()
 				require("catppuccin").setup({
-					flavour = "macchiato", -- latte, frappe, macchiato, mocha
+					flavour = "frappe", -- latte, frappe, macchiato, mocha
 					background = { -- :h background
 						light = "latte",
 						dark = "mocha",
@@ -63,8 +63,130 @@ return {
 				})
 
 				-- setup must be called before loading
-				vim.cmd.colorscheme("catppuccin")
+				-- vim.cmd.colorscheme("catppuccin")
 			end,
 		},
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "moon",
+		},
+		config = function()
+			require("tokyonight").setup({
+				-- vim.cmd.colorscheme("tokyonight"),
+			})
+		end,
+	},
+	{
+		"neanias/everforest-nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("everforest").setup({
+				vim.cmd.colorscheme("everforest"),
+			})
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				--- @usage 'auto'|'main'|'moon'|'dawn'
+				variant = "moon",
+				--- @usage 'main'|'moon'|'dawn'
+				dark_variant = "moon",
+				bold_vert_split = false,
+				dim_nc_background = false,
+				disable_background = false,
+				disable_float_background = false,
+				disable_italics = false,
+
+				--- @usage string hex value or named color from rosepinetheme.com/palette
+				groups = {
+					background = "base",
+					background_nc = "_experimental_nc",
+					panel = "surface",
+					panel_nc = "base",
+					border = "highlight_med",
+					comment = "muted",
+					link = "iris",
+					punctuation = "subtle",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					warn = "gold",
+
+					headings = {
+						h1 = "iris",
+						h2 = "foam",
+						h3 = "rose",
+						h4 = "gold",
+						h5 = "pine",
+						h6 = "foam",
+					},
+					-- or set all headings at once
+					-- headings = 'subtle'
+				},
+
+				-- Change specific vim highlight groups
+				-- https://github.com/rose-pine/neovim/wiki/Recipes
+				highlight_groups = {
+					ColorColumn = { bg = "rose" },
+
+					-- Blend colours against the "base" background
+					CursorLine = { bg = "foam", blend = 10 },
+					StatusLine = { fg = "love", bg = "love", blend = 10 },
+
+					-- By default each group adds to the existing config.
+					-- If you only want to set what is written in this config exactly,
+					-- you can set the inherit option:
+					Search = { bg = "gold", inherit = false },
+				},
+			})
+
+			-- Set colorscheme after options
+			-- vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		setup = true,
+		opts = ...,
+		config = function()
+			-- Default options:
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = true,
+					emphasis = true,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = false,
+			})
+			-- vim.cmd("colorscheme gruvbox")
+		end,
 	},
 }
